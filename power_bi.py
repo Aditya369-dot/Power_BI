deb2aae6e3c4630d8d2cdc95862d6863839ac564
aditya <adityabholla@gmail.com>
2024-03-04 21:18:42 -0800

    Radio functions and data modelling.

--- power_bi.py
+++ power_bi.py
@@ -6,12 +6,25 @@ import imageio
 st.set_page_config(layout="wide")
 
 st.sidebar.info("lets Connect")
+
+
+
 st.sidebar.markdown("""
 [![GitHub](https://img.icons8.com/ios-glyphs/45/FFFFFF/github.png)](https://github.com/Aditya369-dot)
 [![LinkedIn](https://img.icons8.com/ios-glyphs/45/FFFFFF/linkedin-circled--v2.png)](https://www.linkedin.com/in/aditya-b-231a0a194/)
 [![Twitter](https://img.icons8.com/ios-glyphs/40/FFFFFF/twitter.png)](https://twitter.com/adibholla21)
 [![Instagram](https://img.icons8.com/ios-glyphs/45/FFFFFF/instagram-new.png)](https://www.instagram.com/aditya_4real/?next=%2Foauth%2Foidc%2F%3Fapp_id%3D1289884158313322%26scope%3Dopenid%26response_type%3Dcode%26state%3DATB3TBXUaFJLKVN4dV5PYrDMQbVb4gYfCo_NtC7hKYeXi_xGSJiosSqUdnYymaMoKJCIQ-uoXpCCDDfHA95spC5vL_FHjyYUfkdMmj9igbQgB-ULu0BNLWW_VlXz7cGbGkSqtdg-Hl5PNAFvQ3Q4T4KkG1A%26redirect_uri%3Dhttps%3A%2F%2Fwww.threads.net%2Flogin%2Foidc%2F%26logger_id%3D31423f7a-6b01-4808-91a7-32c2a0b89ead)
 """,unsafe_allow_html=True)
+
+filepath = "Power_bi_resume.pdf"
+
+with open(filepath,"rb") as file:
+    btn = st.sidebar.download_button(
+        label="Download Resume",
+        data=file,
+        mime="application/pdf"
+    )
+
 def gradient(color1, color2, color3):
     st.markdown(f'<h1 style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});font-size:40x;border-radius:3%".'
                 f'<span style="color:{color3};">Creating Simple and Powerful Power BI Dashboard using E-commerce data</span><br>',unsafe_allow_html=True)
@@ -136,7 +149,10 @@ with col6:
     st.image("Screenshot model).png", width=1200)
 
 
-options = ["Exec Dashboard", "Model and Calculated Fields", "Map", "Product Detail", "Customer Detail"]
+st.header("🛠️ STEP 1 ➡️ Understanding each Analysis on the Dashboard individually for a better understanding ")
+st.subheader("Choose options below to select one analysis and learn more about it")
+
+options = ["Exec Dashboard", "Model and Calculated Fields",  "Map", "Product Detail", "Customer Detail", "DAX integrations for Drill-downs"]
 
 # Create radio buttons in a horizontal layout
 selected_option = st.radio("Choose an option:", options, horizontal=True)
@@ -157,6 +173,9 @@ elif selected_option == "Product Detail":
 elif selected_option == "Customer Detail":
     st.image("path_to_customer_detail_image.jpg")  # Replace with your image path or URL
     st.write("Description for Customer Detail")
+elif selected_option == "DAX integrations for Drill-downs":
+    st.image("")
+    st.write("")
 
 
 
